6ea79c2658f883a8c6c6e29127ab30da795977d3
Song <217.Songs.SH@gmail.com>
2020-04-23 12:29:11 +0900

    v

--- CH_6/ex4boardgametimer/src/main/java/com/example/ex4boardgametimer/MainActivity.java
+++ CH_6/ex4boardgametimer/src/main/java/com/example/ex4boardgametimer/MainActivity.java
@@ -2,16 +2,21 @@ package com.example.ex4boardgametimer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.nio.channels.OverlappingFileLockException;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     Button btnTimerSwitch, btnReset, btnPauseRestart;
     TextView textViewCopyright;
     CountDownTimer countDownTimer;
+
+    TextToSpeech textToSpeech;
 
     long fullTime, halfTime;
     boolean isPaused = false;
@@ -29,12 +36,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        editTextEnteredSecond = findViewById(R.id.editTextEnteredSeconds);
-        btnTimerSwitch = findViewById(R.id.btnTimerSwitch);
-        btnReset = findViewById(R.id.btnReset);
-        btnPauseRestart = findViewById(R.id.btnPauseRestart);
-        textViewCopyright = findViewById(R.id.textViewCopyright);
+        {
+            editTextEnteredSecond = findViewById(R.id.editTextEnteredSeconds);
+            btnTimerSwitch = findViewById(R.id.btnTimerSwitch);
+            btnReset = findViewById(R.id.btnReset);
+            btnPauseRestart = findViewById(R.id.btnPauseRestart);
+            textViewCopyright = findViewById(R.id.textViewCopyright);
+            textToSpeech = new TextToSpeech(getApplicationContext(), status -> {
+                if(status == TextToSpeech.SUCCESS)
+                    textToSpeech.setLanguage(Locale.ENGLISH);
 
+            });
+        }
 
         btnTimerSwitch.setOnClickListener(v -> {
             MediaPlayer.create(getApplicationContext(), R.raw.bell_sound2).start();
@@ -47,11 +60,20 @@ public class MainActivity extends AppCompatActivity {
         });
 
         btnReset.setOnLongClickListener(view -> {
+            if(editTextEnteredSecond.hasFocus()){
+                editTextEnteredSecond.clearFocus();
+                InputMethodManager imm = (InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(editTextEnteredSecond.getWindowToken(), 0);
+            }
+
             resetTimer(); //타이머 초기화
             cencleTimer(); //타이머 멈추기
 
-            btnPauseRestart.setEnabled(true);
-            return false;
+            String msg = String.format("Reset to %d seconds", fullTime);
+            textToSpeech.speak("",TextToSpeech.QUEUE_FLUSH, null);
+
+            btnTimerSwitch.setEnabled(true);
+            return true;
         });
 
         btnPauseRestart.setOnClickListener(v -> {
@@ -84,7 +106,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTick(long l) {
                 long currentTime = l/1000;
+                changeTestSize(currentTime);
+
+                if(currentTime == halfTime)
+                {
+                    MediaPlayer.create(getApplicationContext(), R.raw.warning_sound);
+                }
+                if(currentTime <= 10)
+                {
+                    btnTimerSwitch.setTextColor(Color.RED);
+                    textToSpeech.speak(String.valueOf(currentTime),TextToSpeech.QUEUE_FLUSH,null);
+                }
                 btnTimerSwitch.setText(String.valueOf(currentTime));
+
             }
 
             @Override
@@ -100,11 +134,37 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void resetTimer(){
+        if(editTextEnteredSecond.getText().equals("")){
+            Toast.makeText(this,"null",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         fullTime = Long.parseLong(editTextEnteredSecond.getText().toString());
         halfTime = Math.round(fullTime/2);
+
+
+        changeTestSize(fullTime);
 
         btnTimerSwitch.setBackgroundColor(Color.YELLOW);
         btnTimerSwitch.setTextColor(Color.BLACK);
         btnTimerSwitch.setText(String.valueOf(fullTime));
+
+
     }
+
+    private void changeTestSize(long seconds)
+    {
+        if(seconds >= 100){
+            btnTimerSwitch.setTextSize(200);
+        }
+        else if(seconds >= 10)
+        {
+            btnTimerSwitch.setTextSize(300);
+        }
+        else {
+            btnTimerSwitch.setTextSize(400);
+        }
+    }
+
+
 }
