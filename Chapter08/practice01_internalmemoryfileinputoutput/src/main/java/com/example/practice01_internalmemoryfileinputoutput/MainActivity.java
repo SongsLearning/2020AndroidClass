63288aa1a7aa68785f09ab00ba11edb4f22d48d9
Song <217.Songs.SH@gmail.com>
2020-05-15 13:07:44 +0900

    Diary

--- Chapter08/practice01_internalmemoryfileinputoutput/src/main/java/com/example/practice01_internalmemoryfileinputoutput/MainActivity.java
+++ Chapter08/practice01_internalmemoryfileinputoutput/src/main/java/com/example/practice01_internalmemoryfileinputoutput/MainActivity.java
@@ -4,6 +4,7 @@ package com.example.practice01_internalmemoryfileinputoutput;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,10 +12,17 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.Buffer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
         btnRead = findViewById(R.id.btnRead);
         textView = findViewById(R.id.textView);
 
+
+        // region
         View.OnClickListener listener1 =new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -85,8 +95,76 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
-        btnWrite.setOnClickListener(listener1);
-        btnRead.setOnClickListener(listener1);
+
+        // endregion
+
+
+        //region
+        View.OnClickListener listener2 = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()){
+                    case R.id.btnWrite:
+                        try (OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput("myFile.txt", Context.MODE_PRIVATE))){
+                          outputStreamWriter.write(editText.getText().toString());
+                          Toast.makeText(MainActivity.this, "complate", Toast.LENGTH_SHORT).show();
+                          editText.setText(null);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    case R.id.btnRead:
+                        try( InputStreamReader inputStreamReader = new InputStreamReader(openFileInput("myFile.txt"))){
+                            String str = "";
+
+                            int temp;
+                            while((temp = inputStreamReader.read())!= -1){
+                                str+=(char)temp;
+                            }
+                            textView.setText(str);
+                        }catch (IOException e){
+                            e.printStackTrace();
+                        }
+
+                        break;
+                }
+            }
+        };
+
+        //endregion
+
+        //region
+
+        View.OnClickListener listener3 = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()){
+                    case R.id.btnWrite:
+
+                        try ( BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(getFileStreamPath("myFile.txt"),false))){
+                           bufferedWriter.write(editText.getText().toString());
+                           editText.setText(null);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+
+                        break;
+                    case R.id.btnRead:
+                        try ( BufferedReader bufferedReader = new BufferedReader(new FileReader(getFileStreamPath("myFile.txt")))){
+                            textView.setText(bufferedReader.readLine());
+
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                }
+            }
+        };
+        //endregion
+        btnWrite.setOnClickListener(listener3);
+        btnRead.setOnClickListener(listener3);
     }
 }
 
