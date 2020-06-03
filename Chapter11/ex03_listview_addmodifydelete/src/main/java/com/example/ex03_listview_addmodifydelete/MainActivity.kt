18a1e2b29d93821b44813d4c88ff17343d0faae9
Song <217.Songs.SH@gmail.com>
2020-06-03 12:10:16 +0900

    fix array list

--- Chapter11/ex03_listview_addmodifydelete/src/main/java/com/example/ex03_listview_addmodifydelete/MainActivity.kt
+++ Chapter11/ex03_listview_addmodifydelete/src/main/java/com/example/ex03_listview_addmodifydelete/MainActivity.kt
@@ -10,33 +10,35 @@ import android.widget.ListView
 import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
 
     var dataArr = getStringArray()
 
     var arrList = getArrayListData()
 
-    val adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_single_choice,arrList)
 
+    private fun getStringArray(): Array<String> {
+
+        return Array(100){ i->i.toString()+"hi"}
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        listView.choiceMode = ListView.CHOICE_MODE_SINGLE
+
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,arrList  )
+
 
         // lastView ArrayAdapter connect
 
-
-        btnAdd.setOnClickListener(this)
-        btnModify.setOnClickListener(this)
-        btnDelete.setOnClickListener(this)
-
+        listView.adapter = adapter
 
         listView.setOnItemClickListener{
             parent, view, position, id ->
-            textView.setText(arrList.get(position))
+            textView.setText(dataArr[position])
         }
+
 
     }
 
@@ -49,43 +51,39 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         return a
     }
 
-    private fun getStringArray(): Array<String> {
-
-        return Array(100){ i->i.toString()+"hi"}
-    }
 
 
-    override fun onClick(v: View?) {
-        val count = adapter.count
-        val checkedItemPostition = listView.checkedItemPosition
-
-        if (v != null) {
-            when(v.id){
-                R.id.btnAdd -> {
-
-                }
-
-                R.id.btnModify -> {
-
-                    val editText = EditText(applicationContext)
-                    AlertDialog.Builder(applicationContext)
-                            .setTitle("listView Item Edit")
-                            .setMessage("select data : " + arrList.get(checkedItemPostition))
-                            .setIcon(R.mipmap.ic_launcher)
-                            .setCancelable(false)
-                            .setView(editText)
-                            .setPositiveButton("Edit", DialogInterface.OnClickListener {
-                                dialog, which -> arrList.set(checkedItemPostition,editText.text.toString())
-                            })
-                            .setNegativeButton("cancle",null)
-                            .show()
-
-                }
-                R.id.btnDelete -> {
-                    arrList.removeAt(checkedItemPostition)
-                }
-            }
-            adapter.notifyDataSetChanged()
-        }
-    }
+//    override fun onClick(v: View?) {
+//        val count = adapter.count
+//        val checkedItemPostition = listView.checkedItemPosition
+//
+//        if (v != null) {
+//            when(v.id){
+//                R.id.btnAdd -> {
+//
+//                }
+//
+//                R.id.btnModify -> {
+//
+//                    val editText = EditText(applicationContext)
+//                    AlertDialog.Builder(applicationContext)
+//                            .setTitle("listView Item Edit")
+//                            .setMessage("select data : " + arrList.get(checkedItemPostition))
+//                            .setIcon(R.mipmap.ic_launcher)
+//                            .setCancelable(false)
+//                            .setView(editText)
+//                            .setPositiveButton("Edit", DialogInterface.OnClickListener {
+//                                dialog, which -> arrList.set(checkedItemPostition,editText.text.toString())
+//                            })
+//                            .setNegativeButton("cancle",null)
+//                            .show()
+//
+//                }
+//                R.id.btnDelete -> {
+//                    arrList.removeAt(checkedItemPostition)
+//                }
+//            }
+//            adapter.notifyDataSetChanged()
+//        }
+//    }
 }
